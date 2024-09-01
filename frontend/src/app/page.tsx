c4667bcd5a2d97ae57676bb22194ab36c21b8e63
arianevc6001 <arianevc6001@gmail.com>
2024-09-02 00:43:58 +0530

    user verify

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+// import Link from 'next/link'; // Use Link for navigation
+import RegisterPage from './Components/Register/RegisterPage'
+import Test from './Components/Test/test'
+// import LoginPage from "./Components/Login/LoginPage";
+
+export default function Home() {
+  return (
+    <>
+      <RegisterPage/>
+
+    </>
+  );
+}
